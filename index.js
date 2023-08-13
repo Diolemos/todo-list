18e7ef4a9d2717921ba73ccbaf24b60ae71ad163
Dio Lemos <pedro.devsc@outlook.com>
2023-08-12 23:07:48 -0300

    Implement localStorage using npm module

--- index.js
+++ index.js
@@ -1,44 +1,34 @@
-import express from "express"
-import bodyParser from "body-parser"
-const app = express()
+import express from "express";
+import bodyParser from "body-parser";
+import { LocalStorage } from "node-localstorage";
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+const localStorage = new LocalStorage("./scratch");
+const port = 3001;
 
-const port = 3001
+app.get("/", (req, res) => {
+  const storedTodos = localStorage.getItem("todos");
+  const todos = storedTodos ? JSON.parse(storedTodos) : [];
+  res.render("index.ejs", { todos });
+});
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs",{todos})
-})
+app.post("/submit", (req, res) => {
+  let newTodo = {
+    task: req.body["newTodo"],
+    creationDate: new Date(),
+    isCompleted: false,
+  };
+  let storedTodos = localStorage.getItem("todos");
+  let todos = storedTodos ? JSON.parse(storedTodos) : [];
 
-app.post("/submit",(req,res)=>{
-   let newTodo = {
-        task: req.body["newTodo"],
-        creationDate: new Date(),
-        isCompleted: false
-    }
-    todos.push(newTodo)
-    res.redirect("/")
-})
+  todos.push(newTodo);
 
-app.listen(port, ()=>{
- console.log('listening on port '+port)
-})
+  localStorage.setItem("todos", JSON.stringify(todos));
+  res.redirect("/");
+});
 
-
-
-let todos = [
-    {task: "Study Excel functions",
-        creationDate: "2023-08-11",
-        isCompleted: false},
-        {task: "Practice password hashing",
-        creationDate: "2023-08-12",
-        isCompleted: false},
-        {task: "Implement SQl login management system ",
-        creationDate: "2023-08-11",
-        isCompleted: false},
-        {task: "Walk the dog",
-        creationDate: "2023-08-11",
-        isCompleted: false}
-
-]
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
