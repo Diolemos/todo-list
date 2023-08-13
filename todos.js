2bf2ab1913824c623d20a500f6cfddc49eb3dd83
Dio Lemos <pedro.devsc@outlook.com>
2023-08-12 21:57:30 -0300

    create placeholder todolist

--- todos.js
+++ todos.js
@@ -1,23 +1,4 @@
-import express from "express"
-import bodyParser from "body-parser"
-const app = express()
-
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const port = 3001
-
-app.get("/",(req,res)=>{
-    res.render("index.ejs",{todos})
-})
-
-
-app.listen(port, ()=>{
- console.log('listening on port '+port)
-})
-
-
-let todos = [
+const todos = [
     {task: "Study Excel functions",
         creationDate: "2023-08-11",
         isCompleted: false},
@@ -32,3 +13,5 @@ let todos = [
         isCompleted: false}
 
 ]
+
+export default todos
